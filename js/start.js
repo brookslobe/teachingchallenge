36bc2a5c11476b858c6e2ca98deca22c8590d5ae
Brooks Lobe <brooks.lobe@gmail.com>
2015-11-23 19:53:35 -0800

    starter code functions like end code after watching tutorial

--- js/start.js
+++ js/start.js
@@ -18,13 +18,44 @@ render();
 
 
 function init() {
+	scene = new THREE.Scene();
 
+	// field of view
+	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000)
+	camera.position.z = 25;
 
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+
+	document.body.appendChild(renderer.domElement);
+
+	// Paramaters: Radius, Diameter, Radial Segments, Tubular Segments
+	geometry = new THREE.TorusGeometry(5, 2, 12, 30);
+
+	//material = new THREE.MeshBasicMaterial({
+	material = new THREE.MeshPhongMaterial({
+		color: 'green'
+	});
+
+	controls = new THREE.OrbitControls(camera, renderer.domElement)
+
+	torus = new THREE.Mesh(geometry, material);
+	scene.add(torus);
+
+	var light = new THREE.PointLight(0xffffff)
+	light.position.set(-100, 200, 100)
+	scene.add(light)
 }
 
 
 function render() { 
+	requestAnimationFrame(render);
 
+	controls.update();
 
+	torus.rotation.x += 0.01;
+	torus.rotation.y += 0.01;
+
+	renderer.render(scene, camera);
 
 } 
